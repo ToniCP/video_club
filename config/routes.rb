d7343e641d2a6e54f89a10cd2aacf0c745021de8
ToniCP <tonicp97@gmail.com>
2019-06-19 13:33:49 -0500

    New Record Format

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  devise_for :users
   resources :orders
   resources :line_items
   resources :carts
+
   root 'store#index', as: 'store_index'
 
   resources :products
