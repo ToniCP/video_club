4ffe02d65927ee836c578e6cd710bf34770f3f31
ToniCP <tonicp97@gmail.com>
2019-06-21 16:30:00 -0500

    Shopping cart corrections

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -1,6 +1,10 @@
 module CurrentCart
   private
     def set_cart
+    #  @cart = Cart.find(session[:cart_id])
+    #rescue Mongoid::Errors::DocumentNotFound
+    #  @cart = Cart.create
+    #  session[:cart_id] = @cart.id
       if !session[:cart_id].nil?
         @cart = Cart.find(session[:cart_id])
       else
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,5 @@
 class LineItem
   include Mongoid::Document
-  field :quantity, type: Integer # <<== Quitar, no va a servir
   field :order_id, type: Integer
 
   belongs_to :order, optional: true
@@ -8,6 +7,6 @@ class LineItem
   belongs_to :cart
 
   def total_price
-    product.price * quantity
+    product.price
   end
 end
